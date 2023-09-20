97bf695136f8cc81f8a8f7ba71e07be625bea86f
Anton-Rundblad <144686843+Anton-Rundblad@users.noreply.github.com>
2023-09-20 11:39:56 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Test
+//
+//  Created by Anton Rundblad on 2023-09-20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
